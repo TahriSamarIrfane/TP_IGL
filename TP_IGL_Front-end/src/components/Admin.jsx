8d3dba8aff2cf6589e61f2c58f95012280379e05
ls_yousfi@esi.dz <ls_yousfi@esi.com>
2023-12-28 00:29:52 +0100

    Admin1

--- TP_IGL_Front-end/src/components/Admin.jsx
+++ TP_IGL_Front-end/src/components/Admin.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import background from "../assets/images/Page-Modérateur.png"
+import React, {useState} from 'react'
+import background from "../assets/images/Page-admin.png"
+import imageS from "../assets/images/SEO-pana.png"
 const Admin = () =>  {
+  const [nav, setNav] =useState(false);
+
+  const handleNav = () =>{
+    setNav(!nav)
+  }
   return (
-    <div  style={{backgroundImage:  `url(${background})`,backgroundRepeat:"no-repeat",backgroundSize:"cover", height:'100vh',width:'100vw' ,}}>
-      <div className='fixed left-[10%] top-[14%] w-[17%] rounded-sm h-[80%] bg-white'>
-       <h5 className='flex fixed justify-center top-[16%] left-[12%] w-[12%] h-[5%] bg-black  rounded-md  text-white font-bold text-2 '>Dashboard</h5>
-        <ul className='fixed top-[25%]'>
-          <li className='py-3 border-b mx-10'>Name</li>
-          <li className='py-3 border-b mx-10'>Name</li>
-          <li className='py-3 mx-10 '>Name</li> 
-        </ul>
-
-      </div>
-      <div className='fixed left-[30%] top-[14%] '>
-      <p className='flex justify-start  text-white font-bold text-2xl'>Dashboard</p>
-      <div className=' fixed w-[60%] rounded-md h-[73%] bg-white opacity-10'>
-
-        </div>
+   <div className= ' bg-black w-ful h-full'>
+     <div className=' w-full h-full bg-no-repeat' style={{backgroundImage: `url(${background})`, backgroundSize: 'contain', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%' , height: '100vh'}}>
+     <div className='flex flex-row justify-center items-center space-x-8 w-full h-full'>
+  <div className='w-[15%] rounded-sm h-[80%] bg-white hidden md:flex ml-20 '>
+    <div className='flex flex-col justify-between'>
+      <div className='px-6 ml-7 mt-4 bg-darkPink rounded-sm text-2 text-white font-bold'>Dashboard</div>
+     <ul className='fixed top-[25%]'>
+      <li className='py-1 border-b-2 mx-10 text-black hover:border-b-darkPink cursor-pointer'>Upload</li>
+      <li className='py-1 border-b-2 mx-10 text-black hover:border-b-darkPink cursor-pointer'>Modérateur</li>
+     </ul>
+     <p className=' mb-6 py-1 mx-10  text-black hover:border-b-darkPink border-b-2 cursor-pointer'>Déconnecter</p>
+    </div>
+  </div>
+  <div className='flex flex-col items-start w-[60%] h-[80%] space-y-2 '>
+    <h5 className='text-white font-bold text-2xl '>Dashboard</h5>
+    <div className="flex flex-col items-center justify-center bg-opacity-10 h-full w-full mt-11 bg-white">
+      <div className="flex flex-col items-center w-full">
+        <input
+          type="url"
+          className="w-[60%] mt-10  rounded-xl"
+          placeholder="Enter URL"
+        />
+        <img className='h-[60%]' src={imageS} alt='/' />
       </div>
     </div>
+  </div>
+</div>
+
+      </div>
+     </div>
+    
+    
   )
 }
 
