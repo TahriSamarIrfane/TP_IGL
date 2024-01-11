b38b3699b953561e33210f1ed475f2b2e0acc53d
unknown <ls_yousfi@esi.com>
2024-01-11 23:42:59 +0100

    newPage

--- TP_IGL_Front-end/src/components/ProfileUser.jsx
+++ TP_IGL_Front-end/src/components/ProfileUser.jsx
@@ -12,7 +12,11 @@ const ProfileUser = () => {
 
   const [ModifierInfo,setModifierInfo] = useState(true);
   const [ModifierPwd, setModifierPwd] = useState(false);
+  const [ModifierPseudo, setModifierPseudo] = useState(false);
 
+  const handleModifierPseudo= () => {
+    setModifierPseudo(!ModifierPseudo);
+  };
 
   const handleModifierInfo= () => {
     setModifierInfo(!ModifierInfo);
@@ -34,22 +38,22 @@ const ProfileUser = () => {
           <img  src={avatar} style={{borderRadius:'50%', height:'40px',width:'40px',objectFit:'cover'}}  alt='/'/>
           </div>
         </div>
-        {/*Go Back Arrow*/}
-        <div className='  mt-2  lg:px-9 '><MdOutlineKeyboardBackspace size={40} /></div>
+        <div className='flex justify-center  w-full h-[75%] '>
+        <div className=' border-4 w-[80%] h-full'>
         {/*My Profile heading*/}
-        <div className='flex justify-center items-center '>
-            <div className=' flex flex-row w-[80%]  '>
-                 <p className='lg:px-[7%] px-[6.5%] lg:text-3xl  text-xl text-center font-bold ' style={{ fontFamily:'tahoma' }}>Mon Profile</p>
+        <div className='flex justify-center items-center lg:mt-5 '>
+            <div className=' flex flex-row  w-full '>
+            <p className='lg:px-[13.8%] py-3 px-[12%] lg:text-3xl text-xl text-center font-bold  ' style={{ fontFamily:'tahoma' }}>Mon Profile</p>
        
-                  {ModifierInfo && (<div className='flex flex-row space-x-2 lg:px-[17%] px-[16%] ml- '>
+            {ModifierInfo && (<div className='flex flex-row space-x-2 lg:px-[4%] py-3 px-[6%] text-center '>
                     <MdOutlinePersonOutline className='mt-1  ' color='DF1477' size={36}/>
-                    <p  className=' text-black mt-2 lg:text-2xl text-100 '>Informations Personnelles</p>  
+                    <p  className=' text-black mt-2 lg:text-2xl  text-100 '>Informations Personnelles</p>  
                
                   </div>
                   )}
-                  {ModifierPwd && (<div className='flex flex-row space-x-2 lg:px-[17%] px-[16%] ml- '>
+                  {ModifierPwd && (<div className='flex flex-row space-x-2 lg:px-[4%] px-[7%] text-center '>
                     <LuKeyRound className='mt-1  ' color='DF1477' size={36}/>
-                    <p  className=' text-black mt-2 lg:text-2xl text-100 '>Modifier Mot de Passe</p>  
+                    <p  className=' text-black mt-2 lg:text-2xl text-[90%] '>Modifier Mot de Passe</p>  
                
                   </div>
                   )}
@@ -57,58 +61,52 @@ const ProfileUser = () => {
             </div></div>
             
 
-        <div className='flex flex-row justify-center items-center mt-[-2%]  h-[80%] '>
+        <div className='flex flex-row justify-center items-center  mt-[-2%]  h-[80%] '>
        
-            <div className='flex flex-col justify-center items-center border-r-black border-r-7   px-[1.4%] w-[40%]'>
+            <div className='flex flex-col justify-center items-center  px-[1.4%] w-[40%]'>
              
-                  <div className='flex p-4 justify-center items-center '>
+                  <div className='flex p-4 justify-center  items-center mt-3 '>
                     <img className='opacity-70 p-2' style={{borderRadius:'50%', height:'120px',width:'120px',objectFit:'cover'}}  src={avatar} alt='/'/>
                         <p className='lg:px-7 font-bold lg:text-xl px-3 text-black text-opacity-80 text-[80%]' style={{position:'absolute'}}>Modifier</p>
                         </div> 
-                   <p className='lg:text-4xl text-xl'>Jacobi</p>
+                   <p className='lg:text-3xl text-xl'>Jacobi</p>
                 <ul className='flex flex-col p-3'>
                  <div className='flex flex-row mx-8 mb-6 space-x-2 py-1'>
                     <MdOutlinePersonOutline className='mt-1  ' color='DF1477' size={20}/>
-                    <li onClick={handleModifierInfo}  className={!ModifierInfo ? ' text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-xl text-10' :  'text-black border-b-2  lg:text-xl text-10 border-b-darkPink'}>Informations Personnelles</li>  
+                    <li onClick={handleModifierInfo}  className={!ModifierInfo ? ' text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-md text-10' :  'text-black border-b-2  lg:text-xl text-10 border-b-darkPink'}>Informations Personnelles</li>  
                  </div>
                 <div className= 'flex flex-row mx-8 mb-6 space-x-2 py-1 '>
                  <LuKeyRound  className='mt-1  ' color='DF1477' size={20}/>
-                  <li onClick={handleModifierPwd} className={!ModifierPwd ?'text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-xl text-10':'text-black border-b-2 border-b-darkPink lg:text-xl text-10'} >Modifier Mot de Passe</li>
+                  <li onClick={handleModifierPwd} className={!ModifierPwd ?'text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-md text-10':'text-black border-b-2 border-b-darkPink lg:text-xl text-10'} >Modifier Mot de Passe</li>
                 </div>
                 <div className='flex flex-row mx-8 mb-6 space-x-2 py-1'>
                 <LuLogOut className='mt-1 ' color='DF1477' size={20}/>
-                <li className=' text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-xl text-10'>Déconnecter</li>
+                <li className=' text-black border-b-2 hover:border-b-darkPink cursor-pointer lg:text-md text-10'>Déconnecter</li>
               </div>
                </ul> 
             </div>
              
              
-                <div className='flex flex-col space-y-2 border-l-grey border-l-2  px-[1%] py-9 w-[55%]' >
+                <div className='flex flex-col space-y-2 border-l-grey border-l-2 px-[1%] py-9 w-[55%]' >
                   
                   {ModifierInfo && (<div className='flex flex-col space-y-8  px-[10%] '>
                     <div className='flex flex-col space-y-2 justify-start '>
-                    <p>Nom</p>
-                    <input
-                    type="text"
-                    className="rounded-md w-[80%] "
-                    placeholder="Jacobi"
-                    />
                     <p>Pseudo</p>
+                    {ModifierPseudo &&(<div>
                     <input
                     type="text"
-                    className="rounded-md w-[80%]"
+                    className="rounded-md w-full"
                     placeholder="Jacobi23"
-                 />
-                    <p>Email</p>
-                    <input
-                    type="email"
-                    className="rounded-md w-[80%]"
-                    placeholder="Jacobi@"
-                    />
+                    /> </div> )}
+                    {!ModifierPseudo && (<div className='border-2 p-2 rounded-md border-grey border-opacity-35'> <p on onClick={handleModifierPseudo}>Jacobi</p></div>
+                  )}
+                    
+                    <p >Email</p>
+                   <div className='border-2 p-2 rounded-md border-grey border-opacity-35'> <p>*******@esi.dz</p></div>
                     </div>
-                    <div className='flex justify-end w-[80%] '>
-                    <button className='p-1 lg:px-6 px-2 bg-darkPink text-center text-white rounded-md '>Enregistrer</button>
-                    </div>
+                    {ModifierPseudo &&( <div className='flex justify-end w-[80%] '>
+                    <button onClick={handleModifierPseudo} className='p-1 lg:px-6 px-2 bg-darkPink text-center text-white rounded-md '>Enregistrer</button>
+                    </div>)}
                 </div>
                   )}
                   {ModifierPwd && (<div className='flex flex-col space-y-8  px-[10%] '>
@@ -143,6 +141,8 @@ const ProfileUser = () => {
             </div>
             
         </div>
+    </div>
+    </div>
     </div>
   )
 }
--- TP_IGL_Front-end/src/main.jsx
+++ TP_IGL_Front-end/src/main.jsx
@@ -6,9 +6,12 @@ import Admin from './components/Admin.jsx'
 import ProfileUser from './components/ProfileUser.jsx'
 import ProfileAdminMod from './components/ProfileAdminMod.jsx'
 import Moderateur from './components/Moderateur.jsx'
+import SignIn from './components/SignIn.jsx'
+import Article from './components/Article.jsx'
+import ModérerArticle from './components/ModérerArticle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProfileAdminMod/>
+    <ModérerArticle/>
   </React.StrictMode>,
 )
